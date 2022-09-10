7a16810cc63c2735aa3301129f55a9f8758e4413
Klim Orlov <flameko-dan@yandex.ru>
2022-09-10 22:23:41 +0300

    feat: general project files

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 8080
+
+
+app.use(cors({
+  origin: [
+    'http://localhost:3006',
+    'http://localhost:3000'
+  ]
+}))
 
 app.get('/api/users', (req, res) => {
   res.send(
