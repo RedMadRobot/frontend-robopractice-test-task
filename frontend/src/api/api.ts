15c628f475130ed5ae8261145859e25d29b4f0da
Klim Orlov <flameko-dan@yandex.ru>
2022-09-10 22:24:43 +0300

    feat: basic api

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,11 @@
+const fetchData = async () => {
+  const response = await fetch("http://localhost:8080/api/users");
+  const json = await response.json();
+  localStorage.setItem("serverData", JSON.stringify(json));
+}
+
+const api = {
+  fetchData
+}
+
+export default api;
