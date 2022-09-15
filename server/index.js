c1821818d02d8d809ffc0615135faab9bc236dec
SergeyK <tigra2004@gmail.com>
2022-09-15 14:41:23 +1100

    add Server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/api/users', (req, res) => {
+  res.send(
+      require('./data.json')
+  )
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
