d00e538ebbf6d1044a712d08708c61f97a291dfd
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-09-19 20:03:31 +0700

    feat: add style for Limit component

--- src/components/ui/Limit/Limit.tsx
+++ src/components/ui/Limit/Limit.tsx
@@ -1,14 +1,15 @@
 import React, { memo } from 'react';
 import { LimitProps } from './types';
+import { Form } from './StyledComponents';
 
 export const Limit = memo(function LimitMemo({ limit, onLimit }: LimitProps) {
   return (
-    <form>
+    <Form>
       <select value={limit} onChange={onLimit}>
         <option defaultValue="25">25</option>
         <option value="15">15</option>
         <option value="10">10</option>
       </select>
-    </form>
+    </Form>
   );
 });
---
+++ src/components/ui/Limit/StyledComponents.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Form = styled.form`
+  margin-top: 40px;
+`;
+
+export { Form };
