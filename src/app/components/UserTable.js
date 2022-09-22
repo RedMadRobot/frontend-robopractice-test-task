70bfbbba6ea00e681c3c08a916e2324037fd28dc
Malyushkina <e.malyushkina@mail.ru>
2022-09-22 19:27:27 +0800

    немного разнесла код

--- src/app/components/UserTable.js
+++ src/app/components/UserTable.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ResizableAntdTable from 'resizable-antd-table';
-import { mappedUsers, getColumns } from '../dataMapper.js';
+import { mappedUsers, getColumns } from '../utils/dataMapper.js';
 
 const UserTable = ({ users }) => {
   const dataSource = mappedUsers(users);
   const columns = getColumns();
 
   return (
-    <ResizableAntdTable
-      dataSource={dataSource}
-      columns={columns}
-      pagination={true}
-      bordered
-         />
+    <ResizableAntdTable dataSource={dataSource} columns={columns} bordered />
   );
 };
 
--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import UserTable from './components/UserTable.js';
 import UserSelect from './components/UserSelect.js';
+import './styles/main.css';
 export const App = () => {
   const [users, setUsers] = useState([]);
   const [value, setValue] = useState();
   const updateData = (value) => {
     setValue(value);
   };
-  
+
   const userData = value
     ? users.filter(({ Fullname }) => Fullname === value)
     : users;
@@ -21,10 +22,16 @@ export const App = () => {
   }, []);
 
   return (
-    <>
-      <h1>User's table</h1>
-      <UserSelect updateData={updateData} users={users} />
+    <section className='main'>
+      <header className='main__header'>
+        <h1 className='main__title'>User's table</h1>
+        <UserSelect
+          updateData={updateData}
+          users={users}
+          className='main__select'
+        />
+      </header>
       <UserTable users={userData} />
-    </>
+    </section>
   );
 };
